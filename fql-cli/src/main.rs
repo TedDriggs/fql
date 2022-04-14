9d4412d5e3c854d2b82a67da963cb2e4dbcebedd
Ted Driggs <ted.driggs@crowdstrike.com>
2022-04-14 13:37:33 -0700

    Add sort-literals command to CLI
    This command demonstrates the parsing of literals.

--- fql-cli/src/main.rs
+++ fql-cli/src/main.rs
@@ -1,5 +1,10 @@
+use std::collections::BTreeSet;
+
 use clap::{Parser, Subcommand};
-use fql::{ast::Expr, parse};
+use fql::{
+    ast::{Expr, Lit},
+    parse,
+};
 
 #[derive(Subcommand)]
 enum Cmd {
@@ -8,6 +13,8 @@ enum Cmd {
     ListProperties,
     /// List the operand values in FILTER (e.g. "true" or "'windows'").
     ListOperands,
+    /// List the unique literal values in FILTER in sorted order by type then value.
+    SortLiterals,
 }
 
 #[derive(Parser)]
@@ -40,6 +47,37 @@ impl Opts {
                     if let Some(operand) = clause.operand() {
                         println!("{}", operand);
                     }
+                }
+            }
+            Cmd::SortLiterals => {
+                let mut bools = BTreeSet::<bool>::new();
+                let mut ints = BTreeSet::<u64>::new();
+                let mut strings = BTreeSet::<String>::new();
+
+                for value in expr
+                    .clauses()
+                    .filter_map(|c| c.operand()?.literal()?.value())
+                {
+                    match value {
+                        Lit::Str(s) => strings.insert(s.value().into_owned()),
+                        Lit::Bool(b) => bools.insert(b.value()),
+                        Lit::Int(i) => match i.value() {
+                            Ok(i) => ints.insert(i),
+                            Err(_) => false,
+                        },
+                    };
+                }
+
+                for b in bools {
+                    println!("{b}");
+                }
+
+                for i in ints {
+                    println!("{i}");
+                }
+
+                for s in strings {
+                    println!("{s}");
                 }
             }
         }
